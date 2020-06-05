f9da2da7c0cc20ce1ea65fd5e38c0db0ee15292f
Daniel Clements <daniel.clements14@gmail.com>
2020-06-05 20:55:39 +0000

    tutor help needed

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 import os
-from flask import Flask
+from os import path 
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+if path.exists('env.py'):
+    import env
+
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "task_manager"
+COLLECTION_NAME = "tasks"
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return ' hello world..... again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['MONGO_DBNAME'] = 'task_manager'
+os.environ['MONGO_URI'] = 'mongodb+srv://root:r00t@cluster0-ucevl.mongodb.net/task_manager?retryWrites=true&w=majority'
