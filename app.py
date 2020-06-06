c9b04b4bf9ea21d9f052adaefe4d5bef609566ec
Daniel Clements <daniel.clements14@gmail.com>
2020-06-06 04:21:20 +0000

    Date not transfering over on edit page

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from os import path 
+from os import path
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
+
 
 if path.exists('env.py'):
     import env
@@ -17,6 +18,10 @@ app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+
 
 mongo = PyMongo(app)
 
@@ -27,6 +32,32 @@ def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update({'_id': ObjectId(task_id)},
+    {
+        'task_name': request.form.get('task_name'),
+        'category_name': request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent': request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_tasks'))
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
